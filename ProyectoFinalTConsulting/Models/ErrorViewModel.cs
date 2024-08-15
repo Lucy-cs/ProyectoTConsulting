54675451417f713584142b47b3a6fd878ba48118
Lucia Cano <lucy_salazar03@hotmail.com>
2024-08-15 16:07:14 -0600

    Primer commit

---
+++ ProyectoFinalTConsulting/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProyectoFinalTConsulting.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
